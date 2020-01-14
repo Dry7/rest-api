434a662b8fb747a9163eba846a49cc0365ceeca9
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-01-14 17:00:43 +0300

    Orders migrations

--- database/migrations/Version20200114092935.php
+++ database/migrations/Version20200114092935.php
@@ -7,14 +7,22 @@ namespace App\Database\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200113112452 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200114092935 extends AbstractMigration
 {
+    public function getDescription() : string
+    {
+        return '';
+    }
+
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE orders (id INT NOT NULL, name VARCHAR(255) NOT NULL, status SMALLINT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE orders (id INT NOT NULL, status SMALLINT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
